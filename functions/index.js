3240db5f7a83aa096cd6451c06bf7ba5e1829c91
lainfox <lainfox@gmail.com>
2018-03-02 19:37:31 +0900

    Hmm

--- functions/index.js
+++ functions/index.js
@@ -2,54 +2,94 @@
 
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
-
-const fetch = require('node-fetch');
+const app = require('express')();
 
 const blockedPhrases = new RegExp(/porn|sexy/);  // No thank you.
 
-/**
- * Returns the response body of the requested url, url should be encoded with encodeURIComponent if there are additional
- * parameters for the requested url.
- *
- * Example request using URL query parameters:
- *   https://us-central1-<project-id>.cloudfunctions.net/cors?url=https%3A%2F%2Fapi.ipify.org%3Fformat%3Djson
- * Example request using request body with cURL:
- *   curl -H 'Content-Type: application/json' \
- *        -d '{"url": "https://api.ipify.org/?format=json"}' \
- *        https://us-central1-<project-id>.cloudfunctions.net/cors
- *
- * This endpoint supports CORS.
- */
-exports.cors = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    console.log('Query:', req.query);
-    console.log('Body:', req.body);
+var webshot = require('webshot');
+const path = require('path');
+const fs = require('fs');
 
-    let url = req.query.url;
-
-    if (!url) {
-      url = req.body.url;
-    }
-
-    if (!url) {
-      res.status(403).send('URL is empty.');
-    }
-
-    console.log('Request:', url);
-
-    // disallow blocked phrases
-    if (url.match(blockedPhrases)) {
-      res.status(403).send('Phrase in URL is disallowed.');
-    }
-
-    fetch(url, {
-      method: req.method,
-      body: req.get('content-type') === 'application/json' ? JSON.stringify(req.body) : req.body,
-      headers: {
-        'Content-Type': req.get('Content-Type'),
-      },
-    })
-    .then(r => r.headers.get('content-type') === 'application/json' ? r.json() : r.text())
-    .then(body => res.status(200).send(body));
-  });
+app.get('/', function(req, res) {
+  res.status(200).send('hello world')
 });
+
+app.get('/test', function (req, res, next) {
+
+  const public_dir = __dirname + '/public';
+  // get url to process
+  // const url = 'https://www.instagram.com/_rotta_/';
+  // const url = 'http://bbs.ruliweb.com/news/read/104869';
+  // const url = 'http://gall.dcinside.com/board/view/?id=superidea&no=137814';
+  const url = 'http://pann.nate.com/talk/341190139';
+
+  console.log('gogo', url)
+  screen(url)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(next);
+
+  res.status(200).send('okok');
+});
+
+
+app.get('/gg', (req, res, next) => {
+  console.log('Before');
+  
+  const url = 'https://www.instagram.com/_rotta_/';
+
+  // REturn image  
+  getData('google.jpg')
+  .then(file => {
+    const filePath = path.resolve(__dirname) + '/google.jpg';
+    setTimeout(() => {
+      fs.unlinkSync(filePath);
+    }, 2000);
+
+    return res.sendFile(filePath)
+  })
+  .catch(err => res.send(err.message))
+});
+
+function getData(fileName, type = 'utf8') {
+  return new Promise(function(resolve, reject){
+    fs.readFile(fileName, type, (err, data) => {
+        err ? reject(err) : resolve(data);
+    });
+  });
+}
+
+const screen = url => {
+  var options = {
+    streamType: 'jpg',
+    renderDelay: 1000,
+    screenSize: {
+      width: 320
+      , height: 480
+    }
+    , shotSize: {
+      width: 320
+      , height: 'all'
+    }
+    , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
+    + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+  };
+
+  return new Promise(function(resolve, reject) {
+    webshot(url, 'google.jpg', options, function(err) {
+      if (err) {
+        reject(err);
+      }
+
+      // screenshot now saved to google.png 
+      console.log('saved!!');
+      resolve('saved');
+    })
+  });
+  // return new Promise(function(resolve, reject) {
+  //   resolve('saved');
+  // });
+}
+
+exports.cors = functions.https.onRequest(app);
